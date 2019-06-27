d44f984b8de3164960edc2a65b6c24f8469491e6
Marco Frisina <marco@frisina.xyz>
2019-06-27 15:27:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -60,7 +60,16 @@ end
   #  return source_array.sum
 
 def reduce_to_all_true(source_array)
-
+  i = 0;
+  newArray = []
+  while source_array[i] do
+    if source_array[i] = false
+      return false
+    else
+      return true
+    end
+    i += 1
+  end
 end
 
   #source_array.all?
